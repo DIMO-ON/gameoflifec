c30a1071c6bbfb69e3d2c84389476ddf7744c172
DIMO-ON <lukia743@gmail.com>
2026-08-07 22:48:55 +0200

    '...'

--- main.c
+++ main.c
@@ -18,21 +18,21 @@
 #define STARTPT (WIN_SIZE/2) - (CELL_PXSIZE*GRID_SIZE)/2
 
 
-void map_state_pixel(int x, int y, int state) {
+void draw_state(int x, int y, int state) {
 	if (state)
 		DrawRectangle(STARTPT + x*CELL_PXSIZE, STARTPT + y*CELL_PXSIZE, CELL_PXSIZE, CELL_PXSIZE, WHITE);
 	else
 		DrawRectangleLines(STARTPT + x*CELL_PXSIZE, STARTPT + y*CELL_PXSIZE, CELL_PXSIZE, CELL_PXSIZE, GRAY);
 }
 
-// int getidx(int x, int y, int xofst, int yofst) {
-int getidx(int x, int y, int xofst, int yofst, int cell) {
+int getidx(int x, int y, int xofst, int yofst) {
+// int getidx(int x, int y, int xofst, int yofst, int cell) {
 	int newx = (x + xofst);
 	int newy = (y + yofst);
 
 	int newidx = newy*GRID_SIZE + newx;
 	int actualidx = y*GRID_SIZE + x;
-	if (cell == 1) printf("[%d]   :: actualpos: (%d, %d), newpos: (%d, %d)\n", cell, x, y, newx, newy);
+	// if (cell == 1) printf("[%d]   :: actualpos: (%d, %d), newpos: (%d, %d)\n", cell, x, y, newx, newy);
 	if (newy >= 0 && newy < GRID_SIZE &&
 		newx >= 0 && newx < GRID_SIZE)
 		return newidx;
@@ -56,8 +56,8 @@ int update_cellstate(int x, int y, int* stategrid, int* newstategrid) {
 	};
 
 	for (int i = 0; i < 8; i++) {
-		// int idx = getidx(x, y, neighbors[i][0], neighbors[i][1]);
-		int idx = getidx(x, y, neighbors[i][0], neighbors[i][1], stategrid[actualidx]);
+		int idx = getidx(x, y, neighbors[i][0], neighbors[i][1]);
+		// int idx = getidx(x, y, neighbors[i][0], neighbors[i][1], stategrid[actualidx]);
 		if (idx >= 0 && idx < GRID_SIZE * GRID_SIZE)
 			count += stategrid[idx];
 	}
@@ -69,13 +69,11 @@ int update_cellstate(int x, int y, int* stategrid, int* newstategrid) {
 		if (count == 3) newstategrid[actualidx] = 1;
 		else newstategrid[actualidx] = 0;
 	}
-}
 
-int checkcells(int *grid) {
-	for (int i = 0; i < GRID_AREA; i++)
-		if (grid[i]) return 1;
-
-	return 0;
+	if (stategrid[actualidx])
+	printf("(x: %d, y: %d) actualstate: %d, newstate: %d, count: %d\n",
+			x, y, stategrid[actualidx], newstategrid[actualidx], count);
+	
 }
 
 
@@ -83,9 +81,9 @@ int main() {
 	int *stategrid = calloc(sizeof(int), GRID_SIZE*GRID_SIZE);
 	int *newstategrid = calloc(sizeof(int), GRID_SIZE*GRID_SIZE);
 	// initiate state grid
-	stategrid[0*GRID_SIZE] = 1;
-	stategrid[1*GRID_SIZE] = 1;
-	stategrid[2*GRID_SIZE] = 1;
+	stategrid[2*GRID_SIZE+GRID_SIZE/2] = 1;
+	stategrid[3*GRID_SIZE+GRID_SIZE/2] = 1;
+	stategrid[4*GRID_SIZE+GRID_SIZE/2] = 1;
 	
     InitWindow(WIN_SIZE,WIN_SIZE, "hello, world");
     SetWindowState(FLAG_VSYNC_HINT); // limite 60 fps
@@ -100,22 +98,27 @@ int main() {
 
         for (int i = 0; i < GRID_SIZE; i++) {
             for (int j = 0; j < GRID_SIZE; j++) {
-				map_state_pixel(j, i, stategrid[i*GRID_SIZE + j]); 
+				draw_state(j, i, stategrid[i*GRID_SIZE + j]); 
             }
         }
 
         EndDrawing();
-		if (checkcells(stategrid)) {
-			WaitTime(3);
 
-			for (int i = 0; i < GRID_SIZE; i++) {
-				for (int j = 0; j < GRID_SIZE; j++) {
-					update_cellstate(j, i, stategrid, newstategrid); 
-				}
+		// if (!IsKeyPressed(KEY_SPACE)) continue;
+		if (IsKeyPressed(KEY_SPACE)) {
+
+		for (int i = 0; i < GRID_SIZE; i++) {
+			for (int j = 0; j < GRID_SIZE; j++) {
+				update_cellstate(j, i, stategrid, newstategrid); 
 			}
-			memcpy(stategrid, newstategrid, GRID_SIZE * GRID_SIZE * sizeof(int));
 		}
 
+		int *tmp = stategrid;
+		stategrid = newstategrid;
+		newstategrid = tmp;
+		}
+		// memcpy(stategrid, newstategrid, GRID_SIZE * GRID_SIZE * sizeof(int));
     }
+
     return 0;
 }
